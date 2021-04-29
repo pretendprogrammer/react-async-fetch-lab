e4d48c61df8ab319a862f190e4423445ab10c0ab
Yosef <shmuli5770@gmail.com>
2021-04-29 18:24:21 -0400

    commit tree 8b28cc3efdea2cbaf2f1349b85ecde9078e7d9ef

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React, { Component } from "react";
+
 // create your App component here
+export default class App extends Component {
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(result => {
+                console.log(result.people)
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.list}
+            </div>
+        )
+    }
+}
